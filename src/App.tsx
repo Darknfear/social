0a964e74d9adf9ea122a44d43a4c0ab2320cbfe1
hieunguyenvan <hieu.nguyenvan@powergateaustralia.com>
2023-12-20 17:24:23 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import "./globals.css";
+import SignForm from "./_auth/forms/SignInForm";
+import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index path="/home" element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/_auth/forms/SignInForm.tsx
@@ -0,0 +1,9 @@
+const SignForm = () => {
+  return (
+    <div>
+      SignIn Form
+    </div>
+  );
+}
+
+export default SignForm;
